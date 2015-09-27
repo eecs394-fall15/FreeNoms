6aa23a2f59806f4e187ba6cbe1f589476659496e
Sophia L Lou <sll411@publicic114.library.northwestern.edu>
2015-09-27 12:47:05 -0500

    fresh first commit

--- app/event/index.js
+++
@@ -1,4 +0,0 @@
-angular.module('event', [
-  /* Declare any module-specific dependencies here */
-  'common'
-]);
--- app/event/scripts/EventModel.js
+++
@@ -1,3 +0,0 @@
-angular
-  .module('event')
-  .constant('Event', supersonic.data.model('Event'));
