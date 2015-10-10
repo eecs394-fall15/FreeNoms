90af7a5c7148a345eca6e9aaa22d704766c28624
sophialou <sophialou@users.noreply.github.com>
2015-10-09 21:15:57 -0500

    hard coded marker and message

--- app/maps/scripts/IndexController.js
+++ app/maps/scripts/IndexController.js
@@ -2,18 +2,33 @@ angular
   .module('maps')
   .controller('IndexController', function($scope, supersonic) {
     // Controller functionality here
-          $scope.position = undefined; 
+        $scope.position = undefined;
+        var map;
        
        $scope.getCurrentLocation = function(){
-        supersonic.device.geolocation.getPosition()
-        .then(function(position){
+             supersonic.device.geolocation.getPosition()
+            .then(function(position){
             $scope.position = position;
         });
            var location = ""+$scope.position.coords.latitude+", "+$scope.position.coords.longitude;
            return location;
     };
-			$scope.$on('mapInitialized', function(event, map) {
-               
-			
-			})
+    
+    
+			$scope.$on('mapInitialized', function(event, eventMap) {
+                
+            map = eventMap;
+            var message = "message here";
+            var contentWindow = new google.maps.InfoWindow({
+                content: message
+            });
+                var eventLocation = new google.maps.LatLng(parseFloat(42.056459), parseFloat(-87.675267)); 
+                var marker = new google.maps.Marker({position: eventLocation, map : map});
+                
+                (function (_contentWindow){
+                    marker.addListener('click', function(){
+                        _contentWindow.open(map, this); 
+                    });
+                })(contentWindow);
+			});
   });
