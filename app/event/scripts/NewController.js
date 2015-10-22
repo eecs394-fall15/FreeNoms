f0dffcd785190676943c02105e661622e856f774
evz9 <evz@u.northwestern.edu>
2015-10-21 23:37:30 -0500

    added hashmap of locations. need to add more

--- app/event/scripts/NewController.js
+++ app/event/scripts/NewController.js
@@ -2,10 +2,17 @@ angular
   .module('event')
   .controller("NewController", function ($scope, Event, supersonic) {
     $scope.event = {};
-
+    var hashMap = {"Tech" : "2145 Sheridan Road, Evanston, IL 60208", 
+      "Ford" : "Ford Motor Company Engineering Design Center, Evanston, IL 60208",
+      "Plex" : "Foster-Walker 1927 Orrington Ave Evanston, IL 60201",
+      "Elder" : "  2400 Sheridan Rd, Evanston, IL 60201",
+      "Allison" : "1820 Chicago Ave Evanston, IL 60201"
+    //  "Lakefill"  
+    };
     $scope.submitForm = function () {
     var location = document.getElementById('tags').value;
         $scope.event.Location = location; 
+        $scope.event.Address = hashMap[location]; 
       $scope.showSpinner = true;
       newevent = new Event($scope.event);
       newevent.save().then( function () {
