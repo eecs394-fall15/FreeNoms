6763b635a3e223cf98d93585feed49bc706363fb
evz9 <evz@u.northwestern.edu>
2015-10-28 17:28:12 -0500

    +code to remove back button after adding new event

--- app/event/scripts/IndexController.js
+++ app/event/scripts/IndexController.js
@@ -3,11 +3,22 @@ angular
   .controller("IndexController", function ($scope, Event, supersonic) {
     $scope.events = null;
     $scope.showSpinner = true;
+    
+    var view = new supersonic.ui.View("maps#index");
 
+    var leftButton = new supersonic.ui.NavigationBarButton( {
+    title: "MapView",
+    onTap: function() {
+      // supersonic.ui.dialog.alert("Left button tapped!");
+      supersonic.ui.layers.push(view);
+      }
+    });
+    
     Event.all().whenChanged( function (events) {
         $scope.$apply( function () {
           $scope.events = events;
           $scope.showSpinner = false;
+          supersonic.ui.navigationBar.update(options);
         });
     });
   });
