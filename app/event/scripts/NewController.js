3c8b8c2fdf8d5a4b63f6d4af1419e57180a65a22
sophialou <sophialou@users.noreply.github.com>
2015-10-27 22:53:53 -0500

    Added locations

--- app/event/scripts/NewController.js
+++ app/event/scripts/NewController.js
@@ -6,7 +6,22 @@ angular
       "Ford" : "Ford Motor Company Engineering Design Center, Evanston, IL 60208",
       "Plex" : "Foster-Walker 1927 Orrington Ave Evanston, IL 60201",
       "Elder" : "  2400 Sheridan Rd, Evanston, IL 60201",
-      "Allison" : "1820 Chicago Ave Evanston, IL 60201"
+      "Allison" : "1820 Chicago Ave Evanston, IL 60201",
+      "Annenberg" : "2120 Campus Drive Evanston, IL 60208",
+      "Annie May Swift": "1920 Campus Drive Evanston, IL 60208", 
+      "Deering Library": "1937 Sheridan Road Evanston, IL 60208", 
+      "Deering Meadow": "1937 Sheridan Road Evanston, IL 60201", 
+      "Fisk": "1845 Sheridan Road Evanston, IL 60208", 
+      "Frances Searle":"2240 Campus Drive Evanston, IL 60208", 
+      "Harris Hall":"1881 Sheridan Road Evanston, IL 60208", 
+      "Locy Hall": "1850 Campus Drive Evanston, IL 60208", 
+      "Lunt":"2033 Sheridan Road Evanston, IL 60208", 
+      "Pick-Staiger":"50 Arts Circle Drive Evanston, IL 60208", 
+      "Sargent":"2245 Sheridan Road Evanston, IL 60201",
+      "Silverman":"2170 Campus Drive Evanston, IL 60208", 
+      "The Rock":"1897 Sheridan Road Evanston, IL 60208", 
+      "University Library":"1970 Campus Drive Evanston, IL 60208",
+      "University Hall": "1897 Sheridan Road Evanston, IL 60208"              
     //  "Lakefill"  
     };
     $scope.submitForm = function () {
